828dec8707f393bda82b4008b0b2daadfe94eac7
Jason Hatfield <100894962+JasonHatfield@users.noreply.github.com>
2022-09-04 15:51:35 -0400

    Add files via upload

---
+++ Program Code/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_Project
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void accessButton_Click(object sender, EventArgs e)
+        {
+            // create a new mainMenu form, show that, and hide this form
+            InventoryProject mainMenu = new InventoryProject();
+            mainMenu.Show();
+            this.Hide();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // close the program
+            this.Close();
+        }
+    }
+}
