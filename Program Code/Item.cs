fc1d9982d3002c0f73f21f466dc8b533d4d8d5eb
Jason Hatfield <100894962+JasonHatfield@users.noreply.github.com>
2022-09-04 12:22:21 -0400

    Add files via upload

---
+++ Program Code/Item.cs
@@ -0,0 +1,57 @@
+﻿// Jason Hatfield
+// This is my original work
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectInventoryLibrary
+{
+    public class Item : Inventory
+    // Inherited class of Inventory
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public int MaxQuantity { get; set; }
+        public bool Show { get; set; }
+        public bool Delete { get; set; }
+
+        public Item() { 
+            Name = "";
+            Type = "";
+            Quantity = 0;
+            MaxQuantity = 0;
+            Show = true;
+            Delete = false;
+        }
+
+        public string DisplayItems
+        // Displays the following format in the list box for each item
+        {
+            get
+            {
+                return $"{ Name } | { Type }";
+            }
+        }
+
+        public string DisplayOnHandStock
+        // Displays the following format in the text box On Hand Stock for each selected item
+        {
+            get
+            {
+                return $"{Quantity}";
+            }
+        }
+
+        public string DisplayMaximumStock
+        // Displays the following format in the text box Maximum Stock for each selected item
+        {
+            get
+            {
+                return $"{MaxQuantity}";
+            }
+        }
+    }
+}
